3f1c2a9b981ab0340111f6e2b03dc011eb6d3465
Vin Ofter <vin.oit99@mail.com>
2023-05-30 17:37:49 +0700

    kiểm tra xem người dùng có thể nhập được từ khóa vào ô tìm kiếm hay không

--- src/test/java/vin0fter/RunTest.java
+++ src/test/java/vin0fter/RunTest.java
@@ -1,8 +1,13 @@
 package vin0fter;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class RunTest {
 
@@ -13,10 +18,28 @@ public class RunTest {
 //        System.setProperty("webdriver.chrome.driver", "")
         WebDriverManager.chromedriver().setup(); //gọi phiên bản browser
         driver = new ChromeDriver(); //khởi tạo giá trị cho browser
-
+        driver.manage().window().maximize();
         driver.get("http://congnongdan.vn/");
 
 
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder=\"Nội dung tìm kiếm...\"]"));
+        searchBox.clear();  // Xóa bất kỳ nội dung nào đã có trong ô tìm kiếm
+        searchBox.sendKeys("nông");  // Nhập từ khóa vào ô tìm kiếm
+
+        searchBox.sendKeys(Keys.RETURN);  // Mô phỏng việc nhấn Enter
+        System.out.println("Nhập dữ liệu thành công");
+
+
+
+
+        WebElement resultsContainer = driver.findElement(By.xpath("//div[@class=\"col-md-12 col-sm-12 padding-right0\"]"));
+//        WebDriverWait wait = new WebDriverWait(driver, 10);
+//        WebElement resultsContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class=\"col-md-12 col-sm-12 padding-right0\"]")));
+        if (resultsContainer.findElements(By.xpath("//div[@class=\"row padding-topbot5\"]")).size() > 0) {
+            System.out.println("Tìm kiếm thành công");
+        } else {
+            System.out.println("Tìm kiếm không thành công");
+        }
 
     }
 }
