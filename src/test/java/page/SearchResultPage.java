8e9c3710aeb75096f8b7356e17221d674eadb6cb
Vin Ofter <vin.oit99@mail.com>
2023-06-29 02:27:24 +0700

    fix bug

--- src/test/java/page/SearchResultPage.java
+++ src/test/java/page/SearchResultPage.java
@@ -3,7 +3,6 @@ package page;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
